bcf721199e2f80d62d8a7cea0f0473e52ab28643
AshunMotley <ashunmotley25@gmail.com>
2025-05-06 13:45:04 -0500

    opens the db

--- code/DBAbstraction.cpp
+++ code/DBAbstraction.cpp
@@ -1,5 +1,31 @@
 #include "DBAbstraction.h"
 #include <iomanip>
 
+using namespace std;
+
+DBAbstraction::DBAbstraction(const string& dbPath)
+{
+    if(!openDB(dbPath))
+    {
+        cerr << "Error opening database";
+    }
+}
+DBAbstraction::~DBAbstraction()
+{
+    closeDB();
+}
+bool DBAbstraction::openDB(const string& dbPath)
+{
+    return sqlite3_open(dbPath.c_str(), &db) == SQLITE_OK;
+}
+void DBAbstraction::closeDB()
+{
+    if(db)
+    {
+        sqlite3_close(db);
+        db = nullptr;
+    }
+}
+
 
 
