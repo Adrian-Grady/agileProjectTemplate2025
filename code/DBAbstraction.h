148d8cd016096ad7864d539426f790dbdd16c657
AshunMotley <ashunmotley25@gmail.com>
2025-05-06 13:27:19 -0500

    added .h

--- code/DBAbstraction.h
+++ code/DBAbstraction.h
@@ -1,8 +1,20 @@
 #pragma once
+#include <iostream>
 #include "sqlite3.h"
+#include <string>
+
 
 using namespace std;
+
 class DBAbstraction
 {
-
+public:
+    DBAbstraction(const string& dbPath);
+    ~DBAbstraction();
+    
+    void getAllStudents(); //prints all the students
+private:
+    sqlite3* db;
+    bool openDB(const string& dbPath);
+    void closeDB();
 };
